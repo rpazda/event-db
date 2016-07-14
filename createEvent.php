b708ea6560e87e7a2065612c8a722e66eb084c0e
rpazda <ri249283@ucf.edu>
2016-07-14 18:57:55 -0400

    Added a bunch of new forms

---
+++ createEvent.php
@@ -0,0 +1,58 @@
+<html>
+
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=windows-1252">
+		
+		<script type="text/javascript" src="jquery-3.0.0.js"></script>
+		<link href="content/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://use.fontawesome.com/808114f81f.js"></script>
+
+		
+	</head>
+	
+	<body>
+	
+		<div class="container-fluid">
+		
+			<div class="row">
+				
+				<div class="col-md-6 col-md-offset-3">
+				
+					<div class="panel panel-primary">
+					
+						<div class="panel-heading">
+							Create New Event
+						</div>
+						
+						<div class="panel-body">
+							
+							<label>Event Type</label>
+							<div class="radio">
+								<label><input type="radio" name="optradio">Public Event</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" name="optradio">Private Event</label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" name="optradio">RSO Event</label>
+							</div>
+							
+							<div class="form-group">
+								<label for="newEvent-name">Event Name</label>
+								<input type="text" class="form-control" id="newEvent-name" placeholder="Event Name">
+							</div>
+							
+							
+							
+						</div>
+					
+					</div>
+					
+				</div>
+				
+			</div>
+		
+		</div>
+		
+	</body>
+</html>
