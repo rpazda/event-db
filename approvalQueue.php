34f64ca6586f0a88ee940db65bf32ad48820efa6
Joshua-L-Casserino <unfunnyone@gmail.com>
2016-07-18 00:30:48 -0400

    I added a list of RSO's that need approval, I have to create the event created page before I can continue.

--- approvalQueue.php
+++ approvalQueue.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
 
 	<head>
@@ -19,10 +22,59 @@
 						<div class="panel-heading">
 							Approve RSOs
 						</div>
-						
-						<div class="panel-body">
-						
-						</div>
+						<form method='POST' />
+							<div class="panel-body">
+								<form method='POST' />
+								<?php
+									
+									$user = 'root';
+									$password = '';
+									$db = 'databaseproject';
+									$link = new mysqli('localhost', $user, $password, $db) or die("Unable to connect!");
+									
+									$sqlQuery = "SELECT * FROM `rsos` WHERE `isApproved` = 0";
+									$result = mysqli_query($link, $sqlQuery);
+									
+									if (mysqli_num_rows($result) > 0) {
+										// output data of each row
+										$i = 0;
+										while($row = mysqli_fetch_array($result)) {
+											$rsoName = $row["rsoName"];
+											$rsoUniversity = $row["universityName"];
+											$rsoAdmin = $row["uid"];
+										  echo "
+											<tr>
+											  <td>
+												<input type='checkbox' name='chk_group[]' value='$rsoName' />  $rsoName from $rsoUniversity by $rsoAdmin<br />
+											  </td>
+											</tr>
+										  ";
+										  $i++;
+										  }
+										  echo "<button class='btn btn-default' type='submit' name='login-button'>Approve RSO</button>";
+									}
+									else {
+										echo " There are no pending RSO requests!<br>";
+									}
+								?>
+							</div>
+						</form>
+						<?php
+							if (isset($_POST['chk_group'])) {
+								$optionArray = $_POST['chk_group'];
+								for ($i=0; $i<count($optionArray); $i++) {
+									$sqlQuery = "UPDATE `rsos` SET `isApproved`= 1 WHERE `rsoName` = '$optionArray[$i]'";
+									$result = mysqli_query($link, $sqlQuery);
+									if($result){
+										echo "<span>";
+										echo $optionArray[$i]." has been approved!<br />";
+										echo "</span>";
+									}
+								}
+								if($result)
+									header('Location: approvalQueue.php');
+							}
+						?>
 					</div>
 				</div>
 			</div>
