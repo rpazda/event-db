5c4f9a57da6145e935d81e061ec8a3fffa883768
Joshua-L-Casserino <unfunnyone@gmail.com>
2016-07-17 21:56:30 -0400

    I fixed the login function.
    There's a problem with double submitting of the registration data. It
    won't add it the database but it tries too.

--- index.php
+++ index.php
@@ -1,134 +1,180 @@
-
+<?php
+session_start();
+?>
 
 <html>
-
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=windows-1252">
 		
+		<script type="text/javascript" src="jquery-3.0.0.js"></script>
 		<link href="content/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://use.fontawesome.com/808114f81f.js"></script>
-		<script src="content/list.min.js"></script>
-
-		
+		<script src="content/scripts/login.js"></script>
 	</head>
 	
-	<body>
-		<?php
-			include_once "header.php";
-		?>
+	<body>	
+	
+		<div id="header"></div>
 	
 		<div class="container-fluid">
-			
 			<div class="row">
-				
-				<div class="col-md-8 col-md-offset-2">
-					
-					<ul class="nav nav-tabs">
-						<li role="presentation" class="active">
-							<a href="#">Day View</a>
-						</li>
-						<li role="presentation">
-							<a href="#">Week View</a>
-						</li>
-						<li role="presentation">
-							<a href="#">Month View</a>
-						</li>
-						<li role="presentation">
-							<a href="#">Year View</a>
-						</li>
-						<li role="presentation">
-							<a href="#">Upcoming</a>
-						</li>
-					</ul>
-					
-					<div class="col-sm-8" id="event-pane">
-					
-						<h2>Events</h2>
+				<div class="col-md-4 col-md-offset-2">
+					<div class="panel panel-primary">	
 						
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								Example Event
-							</div>
-							<div class="panel-body">
-								<p>
-									at <b>00:00am</b> in ABC room 000
-								</p>
-								<p>
-									Join us for nothing never as we celebrate things not happening
-								</p>
-								
-							</div>
+						<div class="panel-heading">
+							Existing Users
 						</div>
 						
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								Example Event
-							</div>
-							<div class="panel-body">
-								<p>
-									at <b>00:00am</b> in ABC room 000
-								</p>
-								<p>
-									Join us for nothing never as we celebrate things not happening
-								</p>
+						<div class="panel-body">
+							<form method="POST" />
+								<div class="form-group">
+									<label for="login-id">User ID</label>
+									<input type="text" class="form-control" name="login-id" placeholder="University Email">
+								</div>
+								<div class="form-group">
+									<label for="login-password">Password</label>
+									<input type="password" class="form-control" name="login-password">
+								</div>
 								
-							</div>
-						</div>
+								<button class="btn btn-default" type="submit" name="login-button">Login</button>								
+							</form>
+							<?php
+								if(!empty($_POST['login-id']) && !empty($_POST['login-password'])){
+									$user = 'root';
+									$password = '';
+									$db = 'databaseproject';
+									$link = new mysqli('localhost', $user, $password, $db) or die("Unable to connect!");
+									$inputUser = $_POST['login-id'];
+									$inputPassword = $_POST['login-password'];
+									$sqlQuery = "SELECT* FROM `users` WHERE `uid` = '$inputUser'";
+									$result = mysqli_query($link, $sqlQuery);
+									$UID = mysqli_fetch_array($result);
+									echo $inputUser;
+									echo "<br>";
+									echo $inputPassword;
+									echo "<br>";
+									echo $UID['uid'];
+									echo "<br>";
+									echo $UID['userPassword'];
+									
+									if ($UID['userPassword'] == $inputPassword){
+										if($UID['userType'] == 0){
+											$_SESSION['userLoggedIn'] = $inputUser;
+											//Send to the admin home page
+										}
+										elseif($UID['userType'] == 2){
+											$_SESSION['userLoggedIn'] = $inputUser;
+											//send to the public events page
+										}
+										else{
+											$_SESSION['userLoggedIn'] = $inputUser;
+											mysqli_close($link);
+											header('Location: home.php');
+										}
+									}
+									else{
+										echo "Login Failed!";
+									}
+								}
+								else if(!empty($_POST['login-id'])){
+									echo "Please enter your password!";
+								}
+								else if(!empty($_POST['login-password'])){
+									echo "Please enter your User ID!";
+								}
+								else{}
+							?>
+
+						</div>	
 						
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								Example Event
-							</div>
-							<div class="panel-body">
-								<p>
-									at <b>00:00am</b> in ABC room 000
-								</p>
-								<p>
-									Join us for nothing never as we celebrate things not happening
-								</p>
-								
-							</div>
-						</div>
-					
 					</div>
-					
-					<div class="col-sm-4">
-					
-						<h2>Calendar</h2>
-						
-						<div class="panel panel-default" style="height: 200px;">
-							<div class="panel-body">Calendar</div>
-						</div>
-						
-						<h3>Filter by RSO</h3>
-						
-							<ul style="list-style-type: none;">
-							
-								<li>
-									<i class="fa fa-flask"></i>&nbsp<a href="#">Chemistry Club</a>
-								</li>
-								
-								<li>
-									<i class="fa fa-music"></i>&nbsp<a href="#">Orchestra</a>
-								</li>
-								
-								<li>
-									<i class="fa fa-sign-language"></i>&nbsp<a href="#">American Sign Language Club</a>
-								</li>
-							
-							</ul>
-					
-					</div>
-				
 				</div>
 				
+				<div class="col-md-4">
+					
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							New Users
+						</div>
+						
+						<div class="panel-body">
+							<form method="POST" />
+								<div class="form-group">
+									<label for="newUser-name">Name</label>
+									<input type="text" class="form-control" name="newUser-name" placeholder="Student Name">
+								</div>
+									
+								<div class="form-group">
+									<label for="newUser-university">University</label>
+										<?php
+											$user = 'root';
+											$password = '';
+											$db = 'databaseproject';
+											$link = new mysqli('localhost', $user, $password, $db) or die("Unable to connect!");
+											$sqlQuery = "SELECT `universityName` FROM `universities` WHERE 1";
+											$result = mysqli_query($link, $sqlQuery);
+											echo "<div class='dropdown'>";
+											echo"<select name='UniversityName'>";
+											echo "<option size =100 ></option>";
+											while($row = mysqli_fetch_array($result)){
+												echo "<option value='".$row['universityName']."'>".$row['universityName']."</option>";
+											}
+											echo "</select>";
+											echo"</div>";
+										?>
+								</div>
+
+								<div class="form-group">
+									<label for="newUser-id">Email</label>
+									<input type="email" class="form-control" name="newUser-id" placeholder="University Email">
+								</div>
+								
+								<div class="form-group">
+									<label for="newUser-password">Password</label>
+									<input type="password" class="form-control" name="newUser-password" placeholder="Password">
+								</div>
+								<button class="btn btn-default" type="submit" name="register-button">Register</button>
+							</form>
+							<?php
+								if(!empty($_POST['newUser-id']) && !empty($_POST['newUser-password']) && !empty($_POST['UniversityName']) && !empty($_POST['newUser-name'])){
+									$user = 'root';
+									$password = '';
+									$db = 'databaseproject';
+									$link = new mysqli('localhost', $user, $password, $db) or die("Unable to connect!");
+									
+									$inputUser = $_POST['newUser-id'];
+									$inputPassword = $_POST['newUser-password'];
+									$inputUniversity = $_POST['UniversityName'];
+									$inputName = $_POST['newUser-name'];
+
+									$sqlQuery = "INSERT INTO users	(uid, userPassword, universityName, userType, userName) VALUES ('$inputUser', '$inputPassword', '$inputUniversity', 1, '$inputName')";
+									$result = mysqli_query($link, $sqlQuery);
+									mysqli_close($link);
+									if ($result)
+									{
+										echo "An account has been resgistered!";
+										echo "Your username is $inputUser";
+									}
+									else{
+										echo "That account is already created!";
+									}
+							}
+							else if(!empty($inputUser) && !empty($inputPassword) && !empty($inputUniversity))
+								echo "Please Enter your name.";
+							else if(!empty($inputUser) && !empty($inputPassword) && !empty($inputName))
+								echo "Please select a University.";
+							else if(!empty($inputUser) && !empty($inputUniversity) && !empty($inputName))
+								echo "Please enter a password.";
+							else if (!empty($inputPassword) && !empty($inputUniversity) && !empty($inputName))
+								echo "Please enter your school email address.";
+							?>
+						</div>	
+					</div>	
+					
+				</div>
 			</div>
-	
 		</div>
 	
-		<?php
-			include_once "footer.php";
-		?>
 	</body>
 	
 </html>
