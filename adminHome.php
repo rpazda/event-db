78e27809ba2bfcec1fe0ec0195bcfc38045a368e
rpazda <richard.pazda@gmail.com>
2016-07-20 01:36:15 -0400

    Little merge

--- adminHome.php
+++ adminHome.php
@@ -10,6 +10,7 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=windows-1252">
 		
+		<script type="text/javascript" src="content/jquery/jquery-3.0.0.min.js"></script>
 		<link href="content/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://use.fontawesome.com/808114f81f.js"></script>
 		<script src="content/scripts/login.js"></script>
